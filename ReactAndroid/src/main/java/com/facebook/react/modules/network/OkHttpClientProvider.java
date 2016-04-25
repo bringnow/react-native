1ad2bb95d7013f5b896476b755fd2720cba54906
Fabian Köster <koesterreich@fastmail.fm>
2016-04-25 14:12:53 +0200

    First attempt to enable TLS 1.2

--- ReactAndroid/src/main/java/com/facebook/react/modules/network/OkHttpClientProvider.java
+++ ReactAndroid/src/main/java/com/facebook/react/modules/network/OkHttpClientProvider.java
@@ -11,9 +11,13 @@ package com.facebook.react.modules.network;
 
 import javax.annotation.Nullable;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
+import com.squareup.okhttp.CipherSuite;
+import com.squareup.okhttp.ConnectionSpec;
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.TlsVersion;
 
 /**
  * Helper class that provides the same OkHttpClient instance that will be used for all networking
@@ -33,7 +37,18 @@ public class OkHttpClientProvider {
 
   private static OkHttpClient createClient() {
     // TODO: #7108751 plug in stetho
+
+    ConnectionSpec spec = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+      .tlsVersions(TlsVersion.TLS_1_2)
+      .cipherSuites(
+        CipherSuite.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+        CipherSuite.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+        CipherSuite.TLS_DHE_RSA_WITH_AES_128_GCM_SHA256)
+      .build();
+
     OkHttpClient client = new OkHttpClient();
+
+    client.setConnectionSpecs(Collections.singletonList(spec));
 
     // No timeouts by default
     client.setConnectTimeout(0, TimeUnit.MILLISECONDS);
